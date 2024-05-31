31b706514a5e1ab4e04543fa09241425e018071e
AndzhurT <andjur38@gmail.com>
2024-05-30 23:31:18 -0400

    Add logic behind display input
    Only numbers are allowed to be on display, you can erase numbers by clicking left arrow

--- script.js
+++ script.js
@@ -39,7 +39,23 @@ let buttonOperations = (e) => {
     e.preventDefault()
     let target = e.target;
     console.log(target.textContent);
-    displayInput.textContent += target.textContent;
+
+    operatorList = ["+", "-", "÷","x"]
+    numberList = ["0","1","2","3","4","5","6","7","8","9"]
+
+    // erase
+    if (target.textContent == "⇚") {
+        let initialWord = displayInput.textContent;
+        let slicedWord = initialWord.slice(0, initialWord.length - 1); 
+        displayInput.textContent = slicedWord;
+    }
+
+    if (numberList.includes(target.textContent)) {
+        displayInput.textContent += target.textContent;
+    }
+    else {
+        console.log("operator");
+    }
 }
 
 btn.forEach(button => {
