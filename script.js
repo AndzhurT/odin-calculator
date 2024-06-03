a1e7349682c0899f9a5859478b2faeb63f7831c2
AndzhurT <andjur38@gmail.com>
2024-06-02 23:23:17 -0400

    Add operators implementation to make the calculator work
    Now you can use all the basic functions of a calculator except module operator, period and negative values.

--- script.js
+++ script.js
@@ -23,17 +23,20 @@ function operate(a, operator, b) {
     else if (operator == "-") {
         return subtract(a, b);
     }
-    else if (operator == "*") {
+    else if (operator == "×") {
         return multiply(a, b);
     }
-    else if (operator == "\//") {
+    else if (operator == "÷") {
         return divide(a, b);
     }
 }
 
 
 const btn = document.querySelectorAll("button");
-const displayInput = document.querySelector(".displayInput");
+const displayCurrInput = document.querySelector(".displayCurrInput");
+const displayPrevInput = document.querySelector(".displayPrevInput");
+const operatorList = ["+", "-", "÷", "×", "="];
+const numberList = ["0","1","2","3","4","5","6","7","8","9"];
 let initialWord;
 
 
@@ -41,22 +44,34 @@ let initialWord;
 let getClearOperation = (initialWord) => {
     if (initialWord.length > 0) {
         initialWord = "0";
-        displayInput.textContent = initialWord;
+        displayCurrInput.textContent = initialWord;
     }
 }
 
-//
-let getOperations = () => {
-
+// Operation button
+let getOperations = (initialWord, target) => {
+    console.log("start")
+    let prevInput = displayPrevInput.textContent.slice(0, displayPrevInput.textContent.length - 1);
+    let currinput = displayCurrInput.textContent;
+    let lastOperator = displayPrevInput.textContent[prevInput.length];
+    console.log("last operator", lastOperator);
+    if (target.textContent == "=" & operatorList.includes(lastOperator)) {
+        displayCurrInput.textContent = operate(prevInput, lastOperator, displayCurrInput.textContent);
+        console.log(operate(prevInput, lastOperator, displayCurrInput.textContent));
+    }
+    else {
+        console.log("finish")
+        displayPrevInput.textContent = initialWord + target.textContent;
+    }
 }
 
 // Number buttons
 let getNumberButtons = (initialWord, target) => {
     if ((initialWord != "0") & (initialWord.length < 15)) {
-        displayInput.textContent += target.textContent;
+        displayCurrInput.textContent += target.textContent;
     }
     else if ((initialWord == "0")) {
-        displayInput.textContent = target.textContent;
+        displayCurrInput.textContent = target.textContent;
     }
 }
 
@@ -64,11 +79,7 @@ let getButtonOperations = (e) => {
     e.preventDefault()
     let target = e.target;
     console.log(target.textContent);
-    initialWord = displayInput.textContent;
-
-    operatorList = ["+", "-", "÷","x"]
-    numberList = ["0","1","2","3","4","5","6","7","8","9"]
-
+    initialWord = displayCurrInput.textContent;
 
     // erase
     if (target.textContent == "C") {
@@ -84,7 +95,7 @@ let getButtonOperations = (e) => {
         else {
             slicedWord = initialWord.slice(0, initialWord.length - 1); 
         }
-        displayInput.textContent = slicedWord;
+        displayCurrInput.textContent = slicedWord;
     }   
 
     // Number buttons
@@ -93,7 +104,7 @@ let getButtonOperations = (e) => {
     }
     
     if (operatorList.includes(target.textContent)) {
-        getOperations
+        getOperations(initialWord, target);
     }
 
     else {
